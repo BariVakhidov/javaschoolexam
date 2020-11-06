d36c0ef6efeb6a41b2c9fa112afae6f3381dd16f
Bary-memesgod <70820908+Bary-memesgod@users.noreply.github.com>
2020-11-07 00:18:28 +0300

    Add files via upload

---
+++ src/main/java/com/tsystems/javaschool/tasks/pyramid/PyramidBuilder.java
@@ -0,0 +1,70 @@
+package com.tsystems.javaschool.tasks.pyramid;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class PyramidBuilder {
+
+    /**
+     * Builds a pyramid with sorted values (with minumum value at the top line and maximum at the bottom,
+     * from left to right). All vacant positions in the array are zeros.
+     *
+     * @param inputNumbers to be used in the pyramid
+     * @return 2d array with pyramid inside
+     * @throws {@link CannotBuildPyramidException} if the pyramid cannot be build with given input
+     */
+    public int[][] buildPyramid(List<Integer> inputNumbers) {
+        try {
+
+
+            if (inputNumbers.contains(null)) {
+                throw new CannotBuildPyramidException();
+            }
+            List<Integer> integerList = inputNumbers.stream().sorted()
+                    .collect(Collectors.toList());
+
+            int rows = 1;
+            int columns = 1;
+            int count = 0;
+
+            while (count < integerList.size()) {
+                count += rows;
+                rows++;
+                columns += 2;
+            }
+            rows -= 1; // необходимое количество строк и столбцов
+            columns -= 2;
+
+            int[][] pyramid;
+            if (count == integerList.size()) {
+                pyramid = new int[rows][columns];
+                for (int[] row : pyramid) {
+                    Arrays.fill(row, 0);
+                }
+            } else {
+                throw new CannotBuildPyramidException();
+            }
+
+            int index = 0;
+            count = 1; //количество цифр в строке
+            int center = columns / 2;
+
+            for (int i = 0, ofs = 0; i < rows; i++, count++, ofs++) {
+                int startNum = center - ofs;
+                for (int j = 0; j < count * 2; j += 2, index++) {
+                    pyramid[i][startNum + j] = integerList.get(index);
+                }
+            }
+            return pyramid;
+        }
+        catch (Exception ex) {
+            ex.printStackTrace();
+            throw new CannotBuildPyramidException();
+        }
+    }
+
+
+}
---
+++ src/main/java/com/tsystems/javaschool/tasks/subsequence/Subsequence.java
@@ -0,0 +1,42 @@
+package com.tsystems.javaschool.tasks.subsequence;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Subsequence {
+
+    /**
+     * Checks if it is possible to get a sequence which is equal to the first
+     * one by removing some elements from the second one.
+     *
+     * @param x first sequence
+     * @param y second sequence
+     * @return <code>true</code> if possible, otherwise <code>false</code>
+     */
+    @SuppressWarnings("rawtypes")
+    public boolean find(List x, List y) {
+        if (x == null || y == null)
+            throw new IllegalArgumentException();
+
+        List listX = new ArrayList(x);
+        List listY = new ArrayList(y);
+
+        listY.retainAll(listX);
+
+        if (listY.size() > listX.size()) {
+            Set setY = new HashSet(listY);
+            if (listX.equals(new ArrayList(setY))) {
+                return true;
+            } else return false;
+        }
+
+        if (listX.equals(listY)) {
+           return true;
+        } else return false;
+
+
+
+    }
+}
